82a5b3170111fb39f42a4011f30dd945b0ca4184
SrGiovanni <samuelefulghi@gmail.com>
2022-03-05 01:49:33 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+// TODO: Create an array of questions for user input
+const questions = ['Please enter the title of your project: (required)',
+                    'Please enter a description of your project:',
+                    'Please enter the installation instructions:',
+                    'Please enter the USage instructions:',
+                    'Please enter contribution instructions',
+                    'Please enter testing instructions'];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
